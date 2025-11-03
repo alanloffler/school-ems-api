3931569c168014dcb7a10a5cb93d4d04cb8621ee
alanloffler <alanmatiasloffler@gmail.com>
2025-11-03 00:30:39 -0300

    feat(auth): implement local guard on signIn controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,15 @@
 import type { Response } from "express";
-import { Controller, Post, Req, Res } from "@nestjs/common";
+import { Controller, Post, Req, Res, UseGuards } from "@nestjs/common";
 
 import type { IRequest } from "@auth/interfaces/request.interface";
 import { AuthService } from "@auth/auth.service";
+import { LocalAuthGuard } from "@auth/guards/local-auth.guard";
 
 @Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @UseGuards(LocalAuthGuard)
   @Post("signIn")
   signIn(@Req() req: IRequest, @Res({ passthrough: true }) res: Response) {
     return this.authService.signIn(req, res);
