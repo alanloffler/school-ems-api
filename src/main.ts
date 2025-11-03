fb478fa5158ac2e8980fd3c19ad91462aac033c3
alanloffler <alanmatiasloffler@gmail.com>
2025-11-02 21:12:31 -0300

    feat(main): global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+
+import { AppModule } from "@/app.module";
 
 async function bootstrap() {
   const PORT: number = parseInt(process.env.PORT ?? "3000");
+
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(PORT);
 }
