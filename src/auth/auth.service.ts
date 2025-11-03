a61236291449f5d48b6374efb294ba151b455c8d
alanloffler <alanmatiasloffler@gmail.com>
2025-11-02 23:38:17 -0300

    feat(auth): sign in function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from "@nestjs/common";
 
+import type { IRequest } from "@auth/interfaces/request.interface";
+import { AdminService } from "@/admin/admin.service";
+
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly adminService: AdminService) {}
+
+  async signIn(req: IRequest, res) {
+    console.log(req.user);
+    // const admin = await this.adminService.findOne({ where: { email } });
+    // return admin;
+  }
+
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.adminService.findOne({ where: { email } });
+
+    if (!user) {
+      throw new HttpException("Invalid credentials", HttpStatus.UNAUTHORIZED);
+    }
+
+    return {
+      id: user.id,
+      email: user.email,
+      role: user.role,
+    };
+  }
+}
