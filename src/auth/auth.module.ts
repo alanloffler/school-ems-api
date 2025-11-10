e4c5444a81342999b81e8b631624beb3df682470
alanloffler <alanmatiasloffler@gmail.com>
2025-11-09 21:45:47 -0300

    feat(auth): jwt module auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,31 @@
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { JwtModule } from "@nestjs/jwt";
 import { Module } from "@nestjs/common";
+import { PassportModule } from "@nestjs/passport";
 
-import { AdminModule } from "@/admin/admin.module";
+import { AdminModule } from "@admin/admin.module";
 import { AuthController } from "@auth/auth.controller";
 import { AuthService } from "@auth/auth.service";
+import { JwtStrategy } from "@auth/strategies/jwt.strategy";
 import { LocalStrategy } from "@auth/strategies/local.strategy";
 
 @Module({
-  imports: [AdminModule],
+  imports: [
+    AdminModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      global: true,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>("JWT_SECRET"),
+        signOptions: {
+          expiresIn: configService.get<any>("JWT_EXPIRES_IN") || "7d",
+        },
+      }),
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, JwtStrategy, LocalStrategy],
 })
 export class AuthModule {}
