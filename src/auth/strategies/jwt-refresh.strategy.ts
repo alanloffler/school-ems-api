3a4410c80a0c8d5e96ddb3238a8d97acb11f7295
alanloffler <alanmatiasloffler@gmail.com>
2025-11-09 22:14:33 -0300

    feat(auth): jwt refresh strategy

---
+++ src/auth/strategies/jwt-refresh.strategy.ts
@@ -0,0 +1,47 @@
+import type { Request } from "express";
+import { ConfigService } from "@nestjs/config";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { HttpException, Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+
+import type { IPayload } from "@/auth/interfaces/payload.interface";
+import { AdminService } from "@/admin/admin.service";
+
+@Injectable()
+export class JwtRefreshStrategy extends PassportStrategy(Strategy, "jwt-refresh") {
+  constructor(
+    configService: ConfigService,
+    private readonly adminService: AdminService,
+  ) {
+    const secret = configService.get<string>("JWT_REFRESH_SECRET");
+    if (!secret) throw new HttpException("JWT_REFRESH_SECRET no está definido", 401);
+
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => {
+          return request?.cookies?.refreshToken;
+        },
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ]),
+      ignoreExpiration: false,
+      secretOrKey: secret,
+      passReqToCallback: true,
+    });
+  }
+
+  async validate(req: Request, payload: IPayload): Promise<IPayload> {
+    try {
+      if (!payload) throw new HttpException("Token invalido", 401);
+
+      const refreshToken = req.cookies.refreshToken;
+      if (!refreshToken) throw new HttpException("Token de refresco no encontrado", 401);
+
+      const admin = await this.adminService.findOne(payload.id);
+      if (admin.data?.refreshToken !== refreshToken) throw new HttpException("Token de refresco no válido", 401);
+
+      return payload;
+    } catch (error) {
+      throw new HttpException("Error al validar el token de refresco", 401);
+    }
+  }
+}
