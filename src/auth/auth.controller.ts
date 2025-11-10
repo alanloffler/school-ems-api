e667762ba786561c84bd4700b43550c26ac72a9d
alanloffler <alanmatiasloffler@gmail.com>
2025-11-09 21:45:12 -0300

    feat(auth): sign in with auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,9 @@
 import type { Response } from "express";
-import { Controller, Post, Req, Res, UseGuards } from "@nestjs/common";
+import { Controller, Get, Post, Req, Res, UseGuards } from "@nestjs/common";
 
 import type { IRequest } from "@auth/interfaces/request.interface";
 import { AuthService } from "@auth/auth.service";
+import { JwtAuthGuard } from "@auth/guards/jwt-auth.guard";
 import { LocalAuthGuard } from "@auth/guards/local-auth.guard";
 
 @Controller("auth")
@@ -13,5 +14,11 @@ export class AuthController {
   @Post("signIn")
   signIn(@Req() req: IRequest, @Res({ passthrough: true }) res: Response) {
     return this.authService.signIn(req, res);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get("signOut")
+  signOut(@Req() req: IRequest, @Res({ passthrough: true }) res: Response) {
+    return this.authService.signOut(req.user, res);
   }
 }
