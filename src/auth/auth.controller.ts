c8ca244191f212d9e2a77d2f245503e201395151
alanloffler <alanmatiasloffler@gmail.com>
2025-11-10 09:44:50 -0300

    feat(auth): refreshToken controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,10 @@
 import type { Response } from "express";
-import { Controller, Get, Post, Req, Res, UseGuards } from "@nestjs/common";
+import { Controller, Get, HttpException, HttpStatus, Post, Req, Res, UseGuards } from "@nestjs/common";
 
 import type { IRequest } from "@auth/interfaces/request.interface";
 import { AuthService } from "@auth/auth.service";
 import { JwtAuthGuard } from "@auth/guards/jwt-auth.guard";
+import { JwtRefreshAuthGuard } from "@auth/guards/jwt-refresh-auth";
 import { LocalAuthGuard } from "@auth/guards/local-auth.guard";
 
 @Controller("auth")
@@ -20,5 +21,14 @@ export class AuthController {
   @Get("signOut")
   signOut(@Req() req: IRequest, @Res({ passthrough: true }) res: Response) {
     return this.authService.signOut(req.user, res);
+  }
+
+  @UseGuards(JwtRefreshAuthGuard)
+  @Post("refreshToken")
+  refreshToken(@Req() req: IRequest, @Res({ passthrough: true }) res: Response) {
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) throw new HttpException("Token de refresco no encontrado", HttpStatus.BAD_REQUEST);
+
+    return this.authService.refreshToken(req.user, refreshToken, res);
   }
 }
